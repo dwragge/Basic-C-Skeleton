9a36606f530a6a79a9856a065a2742ae88a2f4f7
Dylan Wragge <dylanwragge@gmail.com>
2018-06-05 19:15:56 +1200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    cout << "Press any key to continue...";
+    while(!kbhit());
+}
